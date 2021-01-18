1c5369e4437a62b88b4a06a869fc1f03ec36dc9f
Vyacheslav Ponamorev <vy.ponamorev@andersenlab.com>
2021-01-18 17:21:46 +0300

    Added example with using private methods

---
+++ src/main/scala/methods/FindLongLines.scala
@@ -0,0 +1,9 @@
+package methods
+
+object FindLongLines {
+  def main(args: Array[String]): Unit = {
+    val width = args(0).toInt
+    for (arg <- args.drop(1))
+      LongLines.processFile(arg, width)
+  }
+}
